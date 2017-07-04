315b2f839b7a815fc627cc89efbb8369d29c9834
codingdie <547912355@qq.com>
2017-07-04 23:22:08 +0800

    update

--- src/main/java/com/codingdie/leetcode/GroupAnagrams.java
+++ src/main/java/com/codingdie/leetcode/GroupAnagrams.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> lists=new ArrayList<>();
-
-
+        return  lists;
     }
 }
