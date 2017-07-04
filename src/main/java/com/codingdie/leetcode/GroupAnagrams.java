c818a9a50b5feb45fd4485d52790611d21474fc5
xupeng <547912355@qq.com>
2017-07-04 19:58:00 +0800

    GroupAnagrams

---
+++ src/main/java/com/codingdie/leetcode/GroupAnagrams.java
@@ -0,0 +1,15 @@
+package com.codingdie.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shixi_xupeng on 2017/7/4.
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> lists=new ArrayList<>();
+
+
+    }
+}
