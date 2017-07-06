b2a76c5e35df87b8ce465145b251ba65b8213b53
xupeng <547912355@qq.com>
2017-07-06 17:50:15 +0800

    ReverseLinkedListII

---
+++ src/main/java/com/codingdie/leetcode/ReverseLinkedListII.java
@@ -0,0 +1,75 @@
+package com.codingdie.leetcode;
+
+/*
+  Created by xupen on 2017/7/6.
+ */
+public class ReverseLinkedListII {
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(m==n){
+            return head;
+        }
+        ListNode result=head;
+        int i=1;
+        ListNode rhead=null;
+        ListNode begin=null;
+        ListNode end=null;
+        ListNode rEnd=null;
+
+        ListNode pre=null;
+        while (result!=null){
+            ListNode next = result.next;
+            if(i==m){
+                rEnd=result;
+                if(pre!=null){
+                    begin=pre;
+                }
+            }
+            pre=result;
+            if(m<=i&&i<=n){
+                result.next =rhead;
+                rhead=result;
+            }
+            if(i==n){
+                end= next;
+                break;
+            }
+            i++;
+            result= next;
+        }
+        rEnd.next=end;
+        if(m==1){
+            return rhead;
+        }
+        else{
+            begin.next=rhead;
+            return  head;
+        }
+    }
+    public static class  ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+        void print() {
+            ListNode t = this;
+            StringBuffer stringBuffer = new StringBuffer();
+            while (t != null) {
+                stringBuffer.append(t.val + ",");
+                t = t.next;
+            }
+            System.out.println(stringBuffer.substring(0, stringBuffer.length() - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode node1=new ListNode(1);
+        ListNode node2=new ListNode(2);
+        node1.next=node2;
+        ListNode node3=new ListNode(3);
+        node2.next=node3;
+        new ReverseLinkedListII().reverseBetween(node1,2,3).print();
+    }
+}
