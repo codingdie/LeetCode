10c31801d6a48de54fdce0be8c09e09e990577ff
xupeng <547912355@qq.com>
2017-07-11 10:45:05 +0800

    MaximumProductSubarray

--- src/main/java/com/codingdie/leetcode/medium/MaximumProductSubarray.java
+++ src/main/java/com/codingdie/leetcode/medium/MaximumProductSubarray.java
@@ -1,20 +1,29 @@
 package com.codingdie.leetcode.medium;
 
+import java.util.Arrays;
+
 /**
  * Created by xupen on 2017/7/10.
  */
 public class MaximumProductSubarray {
+    //2重动态规划
     public int maxProduct(int[] nums) {
-        int[] dpmax = new int[nums.length];
-        int[] dpmin = new int[nums.length];
-        dpmax[0]=nums[0];
-        dpmin[0]=nums[0];
-
+        int dpmax=nums[0];
+        int dpmin=nums[0];
+        int max=nums[0];
         for (int i = 1; i < nums.length; i++) {
-//           int proMin=dpmin[i-1]*nums[]
+            int dpmax1=Math.max(Math.max(nums[i],nums[i]*dpmax),nums[i]*dpmin);
+            int dpmin1=Math.min(Math.min(nums[i],nums[i]*dpmax),nums[i]*dpmin);
+            if(dpmax1>max){
+                max=dpmax1;
+            }
+            dpmax=dpmax1;
+            dpmin=dpmin1;
         }
-        return 0;
+        return max;
+    }
 
-
+    public static void main(String[] args) {
+        new MaximumProductSubarray().maxProduct(new int[]{-2,3,-4});
     }
 }
